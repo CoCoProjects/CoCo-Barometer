b05364c2e63377a1a504e33d6448ab8af3844e52
Hakan Kayra Doğan <hkayrad@gmail.com>
2023-10-05 21:49:44 +0300

    initial commit

---
+++ CoCo-Barometer.cpp
@@ -0,0 +1,54 @@
+
+/************************************************************************
+																																				Source
+Code Form License Notice
+																								-------------------------------------------
+
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+If it is not possible or desirable to put the notice in a particular
+file, then You may include the notice in a location (such as a LICENSE
+file in a relevant directory) where a recipient would be likely to look
+for such a notice.
+*************************************************************************/
+#include "CoCo-Barometer.h"
+
+Barometer::Barometer(Adafruit_BMP085 &Barometer_REF) : Barometer_REF(Barometer_REF) {
+    Serial.println("Barometer object created.");
+}
+
+void Barometer::startBarometer() {
+    while (!Barometer_REF.begin()) {
+        Serial.println("Barometer initialization failed.");
+        delay(200);
+    }
+}
+
+void Barometer::gatherData() {
+    temperature = Barometer_REF.readTemperature();
+    pressure = Barometer_REF.readPressure();
+    altitude = Barometer_REF.readAltitude();
+    calculatedAltitude = Barometer_REF.readAltitude(SEA_LEVEL_PRESSURE_hPa * 100);
+}
+
+void Barometer::printData() {
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" *C");
+
+    Serial.print("Pressure: ");
+    Serial.print(pressure);
+    Serial.println(" Pa");
+
+    Serial.print("Altitude: ");
+    Serial.print(altitude);
+    Serial.println(" m");
+
+    Serial.print("Calculated Altitude: ");
+    Serial.print(calculatedAltitude);
+    Serial.println(" m");
+
+    Serial.println();
+}
---
+++ CoCo-Barometer.h
@@ -0,0 +1,38 @@
+/************************************************************************
+																																				Source
+Code Form License Notice
+																								-------------------------------------------
+
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+If it is not possible or desirable to put the notice in a particular
+file, then You may include the notice in a location (such as a LICENSE
+file in a relevant directory) where a recipient would be likely to look
+for such a notice.
+*************************************************************************/
+#ifndef COCO_BAROMETER_H
+#define COCO_BAROMETER_H
+
+#include <Arduino.h>
+#include <Adafruit_BMP085.h>
+#include "Wire.h"
+
+//* CONSTANTS
+#define SEA_LEVEL_PRESSURE_hPa 1013.25
+
+class Barometer {
+    private:
+    Adafruit_BMP085 &Barometer_REF;
+    float temperature, altitude, calculatedAltitude;
+    int pressure;
+
+    public:
+    Barometer(Adafruit_BMP085 &);
+    void startBarometer();
+    void gatherData();
+    void printData();
+};
+
+#endif
